8330b0515bf5d30c3f2a8c2db381c6b105649b08
Volius <57324322+JewelDesu@users.noreply.github.com>
2024-10-25 19:18:53 +0300

    Create vid2.tsx

---
+++ webapp/app/vid/vid2.tsx
@@ -0,0 +1,46 @@
+'use client'
+import './styles.css'
+import * as React from "react";
+import Hls from "hls.js";
+
+
+
+export default function Vid2() {
+  const [hls] = React.useState(new Hls());
+  const [hls2] = React.useState(new Hls());
+  const videoEl = React.useRef(null);
+  const videoE2 = React.useRef(null);
+
+  //const source = ['https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8', 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8'];
+  //const video = document.querySelectorAll('video');
+  React.useEffect(() => {
+    if (videoEl.current) {
+      hls.attachMedia(videoEl.current);
+      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        hls.loadSource(
+          "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+        );
+      });
+    }
+  });
+
+  React.useEffect(() => {
+    if (videoE2.current) {
+      hls2.attachMedia(videoE2.current);
+      hls2.on(Hls.Events.MEDIA_ATTACHED, () => {
+        hls2.loadSource(
+          "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+        );
+      });
+    }
+  });
+
+  return (
+    <div className="App" >
+      <video className='vid2' ref={videoEl}  />
+      <video src="admin:admin@192.168.0.113/cgi-bin/mjpg/video.cgi?subtype=1" autoPlay controls />
+    </div>
+  );
+
+
+}
