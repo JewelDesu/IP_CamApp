0cec64b8b08e7063a58ed6923965a2cd00098d1b
Crystic <vilius817@gmail.com>
2024-11-14 21:41:03 +0200

    testing video encoding

--- webapp/app/vid/vid3.tsx
+++ webapp/app/vid/vid3.tsx
@@ -1,15 +1,13 @@
 'use client'
 import './styles.css'
 import * as React from "react";
-import dogVideo from './dog.webm';
 export default function Vid2() {
 
 
 
   return (
     <video width="1200" height="720" controls>
-      <source src="./dog.mp4" type="video/mp4" />
-      <source src={dogVideo} type="video/webm" />
+      <source src="./out2.mp4" type="video/mp4" />
     </video>
   );
 }
